ef12e9d9292df5753d14fb2dd1d6245d4da84c7c
Mateus Rufino Ferreira <163481727+MateusRufinoFerreira@users.noreply.github.com>
2025-03-30 23:41:57 -0300

    Update Triangulo.java

--- lab_4_coesao_e_aclopamento/src/lab_4_coesao_e_aclopamento/Triangulo.java
+++ lab_4_coesao_e_aclopamento/src/lab_4_coesao_e_aclopamento/Triangulo.java
@@ -1,9 +1,13 @@
 package lab_4_coesao_e_aclopamento;
 
-class Triangulo extends FigurasGeometricas {
+public class Triangulo extends FigurasGeometricas {
     private double base, altura, lado1, lado2, lado3;
 
     public Triangulo(double base, double altura, double lado1, double lado2, double lado3) {
+        if (base <= 0 || altura <= 0 || lado1 <= 0 || lado2 <= 0 || lado3 <= 0)
+            throw new IllegalArgumentException("Todos os valores devem ser positivos.");
+        if (!(lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1))
+            throw new IllegalArgumentException("Os lados não formam um triângulo possivel.");
         this.base = base;
         this.altura = altura;
         this.lado1 = lado1;
