6f1dc3fea5ce9388cb5a2fe826671c1c32a182af
Mateus Rufino Ferreira <163481727+MateusRufinoFerreira@users.noreply.github.com>
2025-03-30 23:41:28 -0300

    Update Retangulo.java

--- lab_4_coesao_e_aclopamento/src/lab_4_coesao_e_aclopamento/Retangulo.java
+++ lab_4_coesao_e_aclopamento/src/lab_4_coesao_e_aclopamento/Retangulo.java
@@ -1,9 +1,11 @@
 package lab_4_coesao_e_aclopamento;
 
-class Retangulo extends FigurasGeometricas {
+public class Retangulo extends FigurasGeometricas {
     private double altura, largura;
 
     public Retangulo(double altura, double largura) {
+        if (altura <= 0 || largura <= 0)
+            throw new IllegalArgumentException("Altura e largura devem ser positivas.");
         this.altura = altura;
         this.largura = largura;
     }
